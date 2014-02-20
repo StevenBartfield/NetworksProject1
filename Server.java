f9b1ecfff128426aca03ba51cb0f86b3419d6d31
Steve Bartfield <Stevo393@gmail.com>
2014-02-20 14:13:00 -0600

    Functionality except persistant connection and persistantly open as well

--- Server.java
+++ Server.java
@@ -7,6 +7,7 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Server {
 
@@ -23,63 +24,113 @@ public class Server {
         DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
 
         String strInput;
-        String strPath;
+        String strPath = null;
+        String strRequestType;
 
-        //------------------------------------------------------------------------------
-        //Parsing out request and building the header file
-        //------------------------------------------------------------------------------
+        //SETTING UP SO IT WILL CONSISTANTLY RUN
 
-        //Parse out the requested file from the client request string
-        strInput = in.readLine();
-        int nPathEnd = strInput.indexOf("HTTP") - 1;  //indexes the end of request
-        strPath = strInput.substring(5, nPathEnd);   //pulls the request out
-        //File fileInput = new File(strInput); //keeping in case i need it later -- but can probably remove
+        //while ( (strInput = in.readLine()) != null){
+        //while (in.ready()){
+//            if (!in.ready()){
+//                   strInput = in.readLine();
+//            }
+            //strInput = in.readLine();
+//            if (strInput.equals(null) || strInput.isEmpty() || strInput == ""){
+//                continue;
+//            }
+            strInput = in.readLine();
 
-        //Create the header and send back to client
-        System.out.println(strPath); //for debugging
-        String strHeader = createHeader(strPath);
-        System.out.println(strHeader); //for debugging
-        out.writeBytes(strHeader + "\r\n");
+            //------------------------------------------------------------------------------
+            //Parsing out request and building the header file
+            //------------------------------------------------------------------------------
 
-        //------------------------------------------------------------------------------
-        //Handling the rest of the file request -- will fail if there is no file to be transmitted (404 was delivered)
-        //------------------------------------------------------------------------------
-        try{
-            //get the file per the request and input it into the file stream
-            File fileRequested = new File(strPath);
-            FileInputStream fileOutbound = new FileInputStream(fileRequested);
+            //Parse out the requested file from the client request string
+            //strInput = in.readLine();
+            strRequestType = strInput.substring(0,4); //pulls the request out
 
-            //preparing the file to be transmitted
-            int nByteSize = (int) fileRequested.length();    //find the number bytes in the file
-            byte[] bytFile = new byte[nByteSize];  //putting the file into bytes
-            fileOutbound.read(bytFile);  //read the bytes into a new file to be sent out
+            //check for request type and handle accordingly
+            if (strRequestType.equals("GET ")){
+                strPath = strInput.substring(5, strInput.indexOf("HTTP") - 1);
+            }
+            if (strRequestType.equals("HEAD")){
+                strPath = strInput.substring(6, strInput.indexOf("HTTP") - 1);
+            }
 
-            //Transmit the data to client
-            out.write(bytFile, 0, nByteSize);
-        }catch(Exception e){
-            out.writeBytes("No File!\r\n"); //for 404 requests //find out what should actually be written in the body for 404s.. if anything?
-        }
 
-        //------------------------------------------------------------------------------
-        //Closing off the connection
-        //------------------------------------------------------------------------------
+            System.out.println(strInput);
+            System.out.println(strRequestType);
+            System.out.println(strPath);
 
-        //Ending the connection
-        System.err.println("Connection terminated"); //will play a better role once we get the connection to stay open
-        out.close();
-        in.close();
-        clientSocket.close();
+            //Create the header and send back to client
+            System.out.println(strPath); //for debugging
+            String strHeader = createHeader(strPath, strRequestType);
+            System.out.println(strHeader); //for debugging
+            out.writeBytes(strHeader + "\r\n");
+            System.out.println(strHeader.substring(9,12));
+
+            //------------------------------------------------------------------------------
+            //Handling the rest of the file request -- will fail if there is no file to be transmitted (404 was delivered)
+            //------------------------------------------------------------------------------
+            //only retrieve if a GET file and not a redirect
+            if (strRequestType.equals("GET ") && (!strHeader.substring(9,12).equals("301")))    {
+                try{
+                    //get the file per the request and input it into the file stream
+                    File fileRequested = new File(strPath);
+                    FileInputStream fileOutbound = new FileInputStream(fileRequested);
+
+                    //preparing the file to be transmitted
+                    int nByteSize = (int) fileRequested.length();    //find the number bytes in the file
+                    byte[] bytFile = new byte[nByteSize];  //putting the file into bytes
+                    fileOutbound.read(bytFile);  //read the bytes into a new file to be sent out
+
+                    //Transmit the data to client
+                    out.write(bytFile, 0, nByteSize);
+                }catch(Exception e){
+                    out.writeBytes("No File!\r\n"); //for 404 requests //find out what should actually be written in the body for 404s.. if anything?
+                }
+            }
+            //------------------------------------------------------------------------------
+            //Closing off the connection
+            //------------------------------------------------------------------------------
+
+            //Ending the connection
+            System.err.println("Connection terminated"); //will play a better role once we get the connection to stay open
+            out.close();
+            in.close();
+            clientSocket.close();
+
+        //}   //end while loop
     }
-
 
     //------------------------------------------------------------------------------
     //Methods to help build appropriate headers
     //------------------------------------------------------------------------------
 
     //Method creates the header for the inputted path request
-    public static String createHeader(String strPathInput){
+    public static String createHeader(String strPathInput, String strRequestType){
         String strHeader; //create string variable
+
+        //if not valid request (i.e. POST) send 403 error
+        if (strRequestType.equals("POST")){
+            strHeader =  "HTTP/1.1 403 Forbidden \r\n";
+            return strHeader;
+        }
+
+        //If invalid request, send 501 error
+        if (!strRequestType.equals("GET ") && !strRequestType.equals("HEAD")){
+            strHeader =  "HTTP/1.1 501 Not Implemented \r\n";
+            return strHeader;
+        }
+
         File fileRequested = new File(strPathInput); //gets file requested to find length
+
+        //check if should redirect request
+        String strRedirectPath = findRedirect(strPathInput);
+        if (!strRedirectPath.isEmpty()){
+            strHeader =  "HTTP/1.1 301 Moved Permanetly \r\n";
+            strHeader += "Location: " + strRedirectPath + "\r\n";
+            return strHeader;
+        }
 
         //check if file exists, if not then send 404
         if (!fileRequested.exists()){
@@ -110,5 +161,23 @@ public class Server {
         System.out.println(strContentType); //for debugging
         return strContentType;
     }
+
+    //Checks to see if requested path is a redirect, if so returns redirect path
+    public static String findRedirect(String strPathInput){
+        String strRedirectPath = "";
+        Scanner scanRedirect = null;
+        try {scanRedirect = new Scanner(new File("redirect.defs")); }
+        catch(IOException e){System.out.println("redirect.defs file could not be found" + e.getMessage());}   //only works if server in root
+
+        while (scanRedirect.hasNextLine()){
+            String strRedirect = scanRedirect.nextLine();
+            String[] strSplit = strRedirect.split(" ");
+            if (strSplit[0].equals("/" + strPathInput)){    //added "/" as this is how file is formatted
+                return strSplit[1];
+            }
+        }
+        return strRedirectPath; //no redirect path
+    }
+
 
 }
